fb666cfd083f99cd09db58f72bc3bb3d55552721
hustcc <i@atool.org>
2016-04-18 18:46:27 +0800

    rm used files

--- package.sh
+++
@@ -1,2 +0,0 @@
- ./buildcrx.sh -d . -n chrome-qrcode -p ../atool.crx.pem -e 'crx|package\.sh|buildcrx\.sh|\.git|node_modules|webpack\.config\.js|screenshot\.png|README\.md|package\.json|res|\.gitignore|build|output'
-
